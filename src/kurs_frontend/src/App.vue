6c248e4a9b03fef71458b1bb12af7521fd5e082c
xubuntu <kamil.borkow@wp.pl>
2024-06-25 20:13:47 +0200

    Added button and licznik

--- src/kurs_frontend/src/App.vue
+++ src/kurs_frontend/src/App.vue
@@ -2,5 +2,23 @@
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     Siema!
+    <br/>
+    <button @click="podbij">podbij</button>
+    {{ licznik }}
   </main>
 </template>
+
+<script>
+export default{
+  data(){
+    return{
+      licznik: 0
+    }
+  },
+  methods:{
+    podbij(){
+      this.licznik++
+    }
+  }
+}
+</script>
